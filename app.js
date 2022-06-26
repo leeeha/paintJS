c7c263a6bc63268487745f45a41b8fbff0d742f2
leeeha <jxlhe46@seoultech.ac.kr>
2022-06-26 18:26:49 +0900

    선의 색상과 두께 변경

--- app.js
+++ app.js
@@ -4,11 +4,15 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
 
+// 선의 색상과 두께 변경에 사용 
+const colors = document.getElementsByClassName("jsColor");
+const range = document.getElementById("jsRange");
+
 // 캔버스를 css가 아니라 '픽셀'로 다루려면, 그 크기를 지정해줘야 한다!!! 
 canvas.width = 700;
 canvas.height = 700;
 
-ctx.strokeStyle = "#2c2c2c"; // 선의 색상 
+ctx.strokeStyle = "#2c2c2c"; // 색상 
 ctx.lineWidth = 2.5; // 두께 
 
 let painting = false;
@@ -27,22 +31,27 @@ function onMouseMove(event){
 
     if(!painting){
         // 캔버스 위의 어떤 (x, y)도 path(line)의 시작점이 될 수 있다.  
-        console.log("creating path in ", x, y);
+        //console.log("creating path in ", x, y);
         ctx.beginPath();
         ctx.moveTo(x, y); // path의 시작점 
     }else{
         // 마우스를 클릭한 path의 시작점부터 현재 (x, y)까지 
         // 마우스를 움직이는 내내 선을 그린다. 
-        console.log("creating line in ", x, y);
+        //console.log("creating line in ", x, y);
         ctx.lineTo(x, y);
         ctx.stroke(); // 이걸 해야 선이 그려짐! 
         //ctx.closePath(); // 이걸 하면 시작점이 고정됨. 
     }
-    
 }
 
-function onMouseDown(event){
-    painting = true;
+function handleColorClick(event){
+    const color = event.target.style.backgroundColor;
+    ctx.strokeStyle = color; // 오버라이드 
+}
+
+function handleRangeChange(event){
+    const size = event.target.value;;
+    ctx.lineWidth = size;
 }
 
 if(canvas){
@@ -54,9 +63,11 @@ if(canvas){
     canvas.addEventListener("mouseleave", stopPainting); // 캔버스 벗어나는 경우 
 }
 
+// 각 컬러 버튼을 클릭하면 strokeStyle 색상이 변경되도록 이벤트 처리 
+Array.from(colors).forEach(color => 
+    color.addEventListener("click", handleColorClick)
+);
 
-
-
-
-
-
+if(range){
+    range.addEventListener("input", handleRangeChange);
+}
